5156cd712b8bc4dc81c6aea3f08b8592b00253bc
sanja <sanja@Sanju>
2023-05-27 09:09:54 +0530

    [27th may 23]- modified sample7 class

--- src/StringClass/sample7.java
+++ src/StringClass/sample7.java
@@ -5,7 +5,6 @@ public class sample7
 	public static void main(String[] args) 
 	{
 		String s1="my name is abc";
-		
 		String[] ar = s1.split(" ");    // {my(0)     name(1)      is(2)      abc(3) }
 		
 		System.out.println(ar.length);   //4
